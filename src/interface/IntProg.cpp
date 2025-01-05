cdccc0e2d746ed5d3654abb5164b36bd949899cd
JoD <holkann@gmail.com>
2025-01-05 17:10:14 +0100

    update copyright to 2025

--- src/interface/IntProg.cpp
+++ src/interface/IntProg.cpp
@@ -1,7 +1,7 @@
 /**********************************************************************
 This file is part of Exact.
 
-Copyright (c) 2022-2024 Jo Devriendt, Nonfiction Software
+Copyright (c) 2022-2025 Jo Devriendt, Nonfiction Software
 
 Exact is free software: you can redistribute it and/or modify it under
 the terms of the GNU Affero General Public License version 3 as
