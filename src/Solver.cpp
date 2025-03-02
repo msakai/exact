3a04b42668e661f2c6b985dedec603066e591776
JoD <holkann@gmail.com>
2025-03-02 15:36:07 +0100

    fix compile warnings on some platforms

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1157,7 +1157,7 @@ void Solver::removeSatisfiedNonImpliedsAtRoot() {
   for (int i = lastRemoveSatisfiedsTrail; i < (int)trail.size(); ++i) {
     Lit l = trail[i];
     if (!isOrig(toVar(l))) continue;  // no column view for auxiliary variables for now
-    for (const std::pair<const CRef, int>& pr : lit2cons[l]) {
+    for (std::pair<const CRef, int> pr : lit2cons[l]) {
       Constr& c = ca[pr.first];
       assert(!c.isMarkedForDelete());  // should be erased from lit2cons when marked for delete
       if (c.isSeen()) continue;
@@ -1215,7 +1215,7 @@ void Solver::dominanceBreaking() {
     lit2consOldSize[-l] = col.size();
     Constr* first = &ca[col.cbegin()->first];
     unsigned int firstUnsatIdx = first->getUnsaturatedIdx();
-    for (const std::pair<const CRef, int>& pr : col) {
+    for (std::pair<const CRef, int> pr : col) {
       Constr& c = ca[pr.first];
       unsigned int unsatIdx = c.getUnsaturatedIdx();
       if (unsatIdx < firstUnsatIdx) {  // smaller number of starting lits
@@ -1242,7 +1242,7 @@ void Solver::dominanceBreaking() {
     for (auto it = range.first; it != range.second; ++it) {
       saturating.remove(-it->second);  // not interested in anything that already implies l TODO: is this needed?
     }
-    for (const std::pair<const CRef, int>& pr : col) {
+    for (std::pair<const CRef, int> pr : col) {
       if (saturating.isEmpty()) break;
       Constr& c = ca[pr.first];
       unsigned int unsatIdx = c.getUnsaturatedIdx();
